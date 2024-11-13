31184c13256719c7861b1291901c75593dc57eaf
Shreyas Minocha <sm-git@riseup.net>
2024-11-13 01:41:40 -0500

    Implement number-to-bytestream decoding

--- decode.py
+++ decode.py
@@ -5,6 +5,8 @@ from Crypto.Hash import SHA384
 from Crypto.PublicKey import ECC
 from Crypto.Signature import DSS
 
+from decode_number import decode_number
+
 PUBLIC_KEY = "AwEAA0VDQ1UAAAABAAwxLjMuMTMyLjAuMzQAYwRhBI1vbBVnA1KE/T1UpdQYzG6LLot++cuCP5DdEdeKtedw5G8RKAhU0KbNXVUwym8CSwUyzdAPC98DAgvkJGOZA/x+cnJOWhVvYTqJvy+IlcOgjSe9kqs0O7zEBy26UmvlIw=="
 EXPECTED_CURVE_PARAMS = "1.3.132.0.34"
 SOME_EC_POINT = "04AA87CA22BE8B05378EB1C71EF320AD746E1D3B628BA79B9859F741E082542A385502F25DBF55296C3A545E3872760AB73617DE4A96262C6F5D9E98BF9292DC29F8F41DBD289A147CE9DA3113B5F0B8C00A60B1CE1D7E819D7A431D7C90EA0E5F"
@@ -13,24 +15,44 @@ ANOTHER_CRYPTO_PARAM = "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFC7634D81
 public_key = base64.b64decode(PUBLIC_KEY)
 public_key_preamble, public_key_point = public_key[:28], public_key[30:]
 
-pk = ECC.import_key(public_key_point, curve_name="NIST P-384")
+# FIXME: i think we're importing/constructing the key incorrectly
+# pk = ECC.import_key(public_key_point, curve_name="NIST P-384")
+pk = ECC.construct(
+    curve="NIST P-384",
+    point_x=21768876250334385725747672907467582086806720528308407299377393892072621925580424420270966419451131233829776989815371,
+    point_y=800115138542352385268888331835970223045897418344421089670732162051188097710073126270498232678219200858362384999715,
+)
 print(pk)
 
-verifier = DSS.new(pk, "deterministic-rfc6979", encoding="der")
-# verifier = DSS.new(pk, "fips-186-3", encoding="der")
+# FIXME: not sure which one it's supposed to be
+randfunc = "deterministic-rfc6979"
+# rand_func = "fips-186-3"
+
+verifier = DSS.new(pk, randfunc, encoding="binary")
 
 # print(base64.b64decode(PUBLIC_KEY))
 # print(base64.b64decode(PUBLIC_KEY)[30:].hex())
 
-data = b""
-with open("data.hex") as f:
-    data = bytes.fromhex(f.readline())
+number = ""
+with open("data.dat") as f:  # QR code scan result
+    number = f.read().strip()
 
-expected_signature = b""
-with open("expected-signature.hex") as f:
-    expected_signature = bytes.fromhex(f.readline())
+data = decode_number(number)
 
+expected_signature = (
+    bytes.fromhex("30640230")  # TODO: extract these from `data`
+    + data[1361 + 6 : 1361 + 6 + 48]
+    + bytes.fromhex("0230")  # TODO: extract these from `data`
+    + data[1361 + 6 + 48 : -2]
+)
+print()
+print(data[:1361].hex())
+print()
+print(expected_signature.hex())
 hashed = SHA384.new(data[:1361])
+print()
+print(hashed.digest().hex())
+print()
 
 try:
     verifier.verify(hashed, expected_signature)
@@ -39,7 +61,7 @@ except Exception:
     print("verification failed")
 
 pic = data[15:1199]
-der_encoded_signature = data[1361:]
+# der_encoded_signature = data[1361:]  # FIXME: i don't think it's der-encoded
 compressed_fields = data[1263:-104]
 
 decompressed_fields = zlib.decompress(compressed_fields)
---
+++ decode_number.py
@@ -0,0 +1,90 @@
+from itertools import batched
+
+
+class DecodeNumber:  # u1b
+    """
+    Class that converts the QR code scan result into the bytestream that the rest of the
+    code processes.
+
+    Painstakingly reimplemented from the disassembly of the class "u1.b", especially the
+    "a(II)V" method.
+    """
+
+    def __init__(self):
+        self._a = 0
+        self._b = 0
+        self.buf = []
+
+    def a(self, a, b):
+        if b > 0x20 or b < 0x1:
+            raise Exception("out of range")  # i forgor the range
+
+        v0 = self._b
+
+        v1 = v0 + b
+        v2 = 0x8
+
+        if not (v0 <= 0) or not (v1 < v2):  # before :cond_0
+            v1 = self._a
+            v3 = a << (v0 & 0x1F)
+            v1 |= v3
+            v1 = v1 & 0xFF
+            self._a = v1
+
+            v3 = 0x8 - v0
+            a = a >> (v3 & 0x1F)
+            v2 -= v0
+            b -= v2
+
+            self.buf.append(v1)
+
+            v0 = 0x0
+            self._a = v0
+            self._b = v0
+
+        # cond_0:
+        while True:
+            v0 = b // 0x8
+            if v0 <= 0:
+                break  # :cond_1
+
+            v0 = a & 0xFF
+            self.buf.append(v0)
+
+            a = a >> (0x8 & 0x1F)
+            b = b + (-0x8)
+
+        if b <= 0:  # :cond_1
+            return  # :cond_2
+
+        v0 = self._a
+        v1 = self._b
+
+        a = a << (v1 & 0x1F)
+        a |= v0
+        a = a & 0xFF
+
+        self._a = a
+
+        v1 += b
+        self._b = v1
+
+
+def decode_number(number: str) -> bytes:
+    u1b_instance = DecodeNumber()
+    # this can be something else in some circumstances
+    # maybe if instead of a "singlecode" it's a "multicode" or something?
+    bval = 0xD
+
+    for t in batched(number, 4):
+        chunk = "".join(t)
+        aval = int(chunk)
+
+        u1b_instance.a(aval, bval)
+
+    v0 = u1b_instance._b
+    if not (v0 <= 0):
+        v0 = u1b_instance._a
+        u1b_instance.buf.append(v0)
+
+    return bytes(u1b_instance.buf)
